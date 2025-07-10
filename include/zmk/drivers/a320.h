7f1c69cfb85bfcba432b92a0b4a93dceeb90fe78
daliben123 <2826526432@qq.com>
2025-07-10 08:24:12 +0800

    Update a320.h

--- include/zmk/drivers/a320.h
+++ include/zmk/drivers/a320.h
@@ -1,23 +1,8 @@
-/* 文件名：drivers/touchpad/touchpad_a320.h */
 #pragma once
 
 #include <zephyr/device.h>
-#include <zephyr/drivers/i2c.h>
-#include <zephyr/drivers/gpio.h>
-#include <zephyr/input/input.h>
 
-#define DT_DRV_COMPAT pixart_a320
+#define TRACKPAD_MODE_NORMAL 0
+#define TRACKPAD_MODE_SCROLL 1
 
-/* 设备配置结构 */
-struct touchpad_config {
-    struct i2c_dt_spec bus;          // I2C总线配置[8](@ref)
-    struct gpio_dt_spec motion_gpio; // 运动中断引脚[8](@ref)
-    struct gpio_dt_spec reset_gpio;  // 可选复位引脚[8](@ref)
-};
-
-/* 设备数据结构 */
-struct touchpad_data {
-    struct gpio_callback motion_cb;
-    struct k_work work;
-    const struct device *input_dev;  // 输入设备指针[4](@ref)
-};
+void trackpad_set_mode(const struct device *dev, uint8_t mode);
