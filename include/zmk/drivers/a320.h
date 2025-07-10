0c653e711804daa8cbbe1dc0bbf9682bdecb4328
daliben123 <2826526432@qq.com>
2025-07-10 08:36:44 +0800

    Update a320.h

--- include/zmk/drivers/a320.h
+++ include/zmk/drivers/a320.h
@@ -1,8 +1,19 @@
-#pragma once
+#ifndef ZEPHYR_DRIVER_TRACKPAD_H_
+#define ZEPHYR_DRIVER_TRACKPAD_H_
 
 #include <zephyr/device.h>
+#include <zephyr/input/input.h>
 
-#define TRACKPAD_MODE_NORMAL 0
-#define TRACKPAD_MODE_SCROLL 1
+/* 模式定义 */
+enum trackpad_mode {
+    TRACKPAD_MODE_POINTER,  // 指针模式
+    TRACKPAD_MODE_SCROLL    // 滚动模式
+};
 
+/* 驱动API */
 void trackpad_set_mode(const struct device *dev, uint8_t mode);
+
+/* 设备树兼容性标识 */
+#define DT_TRACKPAD_COMPAT "vendor,trackpad"
+
+#endif /* ZEPHYR_DRIVER_TRACKPAD_H_ */
